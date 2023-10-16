ee8db6ad852344d37bd6c02839c6ee809aa5f162
Atinuke <atinukebidemi@gmail.com>
2023-10-16 16:24:44 +0100

    I practised some JS basics

---
+++ script.js
@@ -0,0 +1,55 @@
+/*
+//this is a calculator to add two numbers
+let x = 1000;
+let y = 2000;
+let total = x + y;
+console.log(total)
+
+//this is a calculator to subtract two numbers
+let a = 5000; b = 4000;
+let output = a - b;
+console.log(output)
+
+//this is a calculator to multiply two numbers
+let p = 10; q = 85;
+let result = p * q;
+console.log(result)
+
+//this is a calculator to calculate area of a triangle
+let height = 10; base = 5;
+const area = height * base/2
+console.log("Area of Triangle = " + area)
+
+//this is a calculator to calculate circumference of a circle
+let pi = 3.14; r = 5;
+const circumference = 2 * pi * r
+console.log("The circumference of a circle = " + circumference) */
+
+//This function adds two numbers
+function addition (x,y){
+    let add= x + y
+    return add
+}
+
+console.log(addition(600,400))
+
+//This function multiplies two numbers
+function multiply (a,b){
+    let multiply= a * b
+    return multiply
+}
+
+console.log(multiply(200,10))
+
+//This function subtracts two numbers
+function subtract (p,q){
+    let subtract= p - q
+    return subtract
+}
+
+console.log (subtract(800,400))
+
+
+
+
+
